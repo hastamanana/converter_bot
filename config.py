2faf84dc9cc147c9d3e29904bff302302ae5ced3
hastamanana <86929694+hastamanana@users.noreply.github.com>
2023-04-04 23:20:23 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+TOKEN = '5979734038:AAG9sELSubIx14bNFPSXz0phdgFoa8nWeJ8' # Токен бота
+
+# Для extesions.py
+keys = {
+    'BTC': 'BTC',
+    'ETH': 'ETH',
+    'USD': 'USD',
+    'EUR': 'EUR',
+    'RUB': 'RUB',
+}
+
+# Для main.py
+keys2 = {
+    'Биткоин': 'BTC',
+    'Эфириум': 'ETH',
+    'Доллар': 'USD',
+    'Евро': 'EUR',
+    'Рубль': 'RUB',
+}
